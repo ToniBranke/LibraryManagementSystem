0ad532b9ce9825fb9209a5c89c34199e9c8bb54d
Tbranke <tbranke@hs-mittweida.de>
2025-08-10 09:49:30 +0200

    books.db
    books.db created and first attempts to fill it

--- code/LibraryManagementSystem/src/Main.java
+++ code/LibraryManagementSystem/src/Main.java
@@ -2,7 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main
 {
+    public static void connect()
+    {
+        var urlBooks = "jdbc:sqlite:/Users/tonibranke/Desktop/wwwww/LibraryManagementSystem/code/LibraryManagementSystem/DB/Books.db";
+        
+    }
+
     public static void main(String[] args)
     {
+
     }
 }
