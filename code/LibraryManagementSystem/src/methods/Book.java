70f3761c331fc1d7a46c67605ac8fec08bf4f1bf
Tbranke <tbranke@hs-mittweida.de>
2025-08-12 14:16:54 +0200

    books & main
    example dataset added and begun to add the menu logic

--- code/LibraryManagementSystem/src/methods/Book.java
+++ code/LibraryManagementSystem/src/methods/Book.java
@@ -1,4 +1,34 @@
 package methods;
 
-public class Book {
+import java.util.Scanner;
+
+public class Book
+{
+    public static void addBook()
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter ISBN:");
+        String isbn = sc.nextLine();
+
+        System.out.println("Title:");
+        String title = sc.nextLine();
+
+        System.out.println("Author:");
+        String author = sc.nextLine();
+
+        System.out.println("publisher:");
+        String publisher = sc.nextLine();
+
+        System.out.println("Genre:");
+        String genre = sc.nextLine();
+
+        System.out.println("publishing Date: (DD-MM-YYYY)");
+        String publishingDate = sc.nextLine();
+
+        System.out.println("language:");
+        String language = sc.nextLine();
+
+        System.out.println("ISBN:");
+    }
 }
